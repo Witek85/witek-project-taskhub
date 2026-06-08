8da752fc237189fbc2d13cf43be96e322329a160
Witek85 <witold.szklarski@gmail.com>
2026-06-08 16:27:59 +0200

    cors fix

--- backend/src/main/java/pl/witold/taskhub/config/WebConfig.java
+++ backend/src/main/java/pl/witold/taskhub/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://51.83.154.177"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
